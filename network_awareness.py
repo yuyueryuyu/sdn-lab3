26e4503e9070ef2f4119249891c4b2d9af2f94d5
Kaiyue Yu <yuyueryuyu@outlook.com>
2025-04-26 23:05:54 +0800

    Complete

--- network_awareness.py
+++ network_awareness.py
@@ -2,7 +2,7 @@ from os_ken.base import app_manager
 from os_ken.base.app_manager import lookup_service_brick
 from os_ken.ofproto import ofproto_v1_3
 from os_ken.controller.handler import set_ev_cls
-from os_ken.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER, DEAD_DISPATCHER
+from os_ken.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER, DEAD_DISPATCHER, HANDSHAKE_DISPATCHER
 from os_ken.controller import ofp_event
 from os_ken.lib.packet import packet
 from os_ken.lib.packet import ethernet, arp
@@ -12,7 +12,6 @@ from os_ken.topology.api import get_host, get_link, get_switch
 from os_ken.topology.switches import LLDPPacket
 
 import networkx as nx
-import copy
 import time
 
 
@@ -32,8 +31,10 @@ class NetworkAwareness(app_manager.OSKenApp):
         self.port_info = {}  # dpid: (ports linked hosts)
         self.topo_map = nx.Graph()
         self.topo_thread = hub.spawn(self._get_topology)
-
-
+        self.echo_thread = hub.spawn(self._get_delay)
+        self.lldp_delay = {}
+        self.echo_delay = {}
+        self.switches = None
         self.weight = 'hop'
 
 
@@ -45,6 +46,20 @@ class NetworkAwareness(app_manager.OSKenApp):
         inst = [parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions)]
         mod = parser.OFPFlowMod(datapath=dp, priority=priority, match=match, instructions=inst)
         dp.send_msg(mod)
+
+    def send_echo_request(self, datapath):
+        ofp_parser = datapath.ofproto_parser
+        now = time.time()
+        req = ofp_parser.OFPEchoRequest(datapath, str(now).encode())
+        datapath.send_msg(req)
+    
+    @set_ev_cls(ofp_event.EventOFPEchoReply, MAIN_DISPATCHER)
+    def echo_reply_handler(self, ev):
+        now = time.time()
+        msg = ev.msg
+        dpid = msg.datapath.id
+        start = float(msg.data.decode())
+        self.echo_delay[dpid] = max(now - start, 0)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -65,8 +80,54 @@ class NetworkAwareness(app_manager.OSKenApp):
         if ev.state == MAIN_DISPATCHER:
             self.switch_info[dpid] = dp
 
-        if ev.state == DEAD_DISPATCHER:
+        if ev.state == DEAD_DISPATCHER and dpid in self.switch_info:
             del self.switch_info[dpid]
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_hander(self, ev):
+        msg = ev.msg
+        dpid = msg.datapath.id
+        try:
+            src_dpid, src_port_no = LLDPPacket.lldp_parse(msg.data)
+            if self.switches is None:
+                self.switches = lookup_service_brick('switches')
+            for port in self.switches.ports.keys():
+                if src_dpid == port.dpid and src_port_no == port.port_no:
+                    self.lldp_delay[src_dpid, dpid] = max(self.switches.ports[port].delay, 0)
+        except:
+            return
+    
+    def _get_delay(self):
+        '''
+        发送echo请求，并获取链路delay
+        '''
+        while True:
+            # 遍历交换机，发送echo request
+            for dp in self.switch_info.values():
+                self.send_echo_request(dp)
+                # 每发送一个echo request暂停一段时间
+                hub.sleep(SEND_ECHO_REQUEST_INTERVAL)
+            
+            # 遍历链路的每条边
+            for src, dst in self.topo_map.edges:
+                # 如果是包括主机的链路，跳过。
+                if self.topo_map[src][dst]['is_host']:
+                    continue
+                try:
+                    # 按照公式计算链路时延。
+                    lldp_delay_s12 = self.lldp_delay[src, dst]
+                    lldp_delay_s21 = self.lldp_delay[dst, src]
+                    echo_delay_s1 = self.echo_delay[src]
+                    echo_delay_s2 = self.echo_delay[dst]
+                    delay = (lldp_delay_s12 + lldp_delay_s21 - echo_delay_s1 - echo_delay_s2) / 2
+                    delay = max(delay, 0)
+                    self.topo_map[src][dst]['delay'] = delay
+                except:
+                    continue
+            if self.weight == 'delay':
+                self.show_topo_map()
+            hub.sleep(GET_DELAY_INTERVAL)
+
     def _get_topology(self):
         _hosts, _switches, _links = None, None, None
         while True:
@@ -76,6 +137,8 @@ class NetworkAwareness(app_manager.OSKenApp):
 
             # update topo_map when topology change
             if [str(x) for x in hosts] == _hosts and [str(x) for x in switches] == _switches and [str(x) for x in links] == _links:
+                # 在continue的时候也要暂停一段时间。
+                hub.sleep(GET_TOPOLOGY_INTERVAL)
                 continue
             _hosts, _switches, _links = [str(x) for x in hosts], [str(x) for x in switches], [str(x) for x in links]
 
@@ -101,11 +164,80 @@ class NetworkAwareness(app_manager.OSKenApp):
 
                 self.link_info[(link.src.dpid, link.dst.dpid)] = link.src.port_no
                 self.link_info[(link.dst.dpid, link.src.dpid)] = link.dst.port_no
+                
+
                 self.topo_map.add_edge(link.src.dpid, link.dst.dpid, hop=1, is_host=False)
 
             if self.weight == 'hop':
                 self.show_topo_map()
             hub.sleep(GET_TOPOLOGY_INTERVAL)
+    
+    # network_awareness类的port_status_handler，在控制器主类的方法中调用
+    def port_status_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+        port = msg.desc.port_no
+        reason = msg.reason
+        ofproto = dp.ofproto
+        
+        # 如果是端口失效
+        if reason == ofproto.OFPPR_DELETE or (reason == ofproto.OFPPR_MODIFY and msg.desc.state in [ofproto.OFPPS_LINK_DOWN, ofproto.OFPPS_BLOCKED]):
+
+            # 在link_info中删除对应端口的链路
+            to_discard = []
+            for s1, s2 in self.link_info:
+                if s1 == dp.id and self.link_info[s1, s2] == port:
+                    to_discard.append((s1, s2))
+            for key in to_discard:
+                self.link_info.pop(key, None)
+            
+            # 在port_link, topo_map中删除包含对应端口的链路
+            to_discard = []
+            for s1, p in self.port_link:
+                _, s2 = self.port_link[s1, p]
+                if s1 == dp.id and p == port:
+                    if self.topo_map.has_edge(s1, s2):
+                        self.topo_map.remove_edge(s1, s2)
+                    self.lldp_delay.pop((s1, s2), None)
+                    self.lldp_delay.pop((s2, s1), None)
+                    to_discard.append((s1, port))
+            for key in to_discard:
+                self.port_link.pop(key, None)
+            
+            # 在port_info中删除对应链路
+            self.port_info[dp.id].discard(port)
+        
+        # 如果是新增端口
+        else:
+            # 重新发现链路
+            hosts = get_host(self)
+            switches = get_switch(self)
+            links = get_link(self)
+            for switch in switches:
+                self.port_info.setdefault(switch.dp.id, set())
+                # record all ports
+                for port in switch.ports:
+                    self.port_info[switch.dp.id].add(port.port_no)
+
+            for host in hosts:
+                # take one ipv4 address as host id
+                if host.ipv4:
+                    self.link_info[(host.port.dpid, host.ipv4[0])] = host.port.port_no
+                    self.topo_map.add_edge(host.ipv4[0], host.port.dpid, hop=1, delay=0, is_host=True)
+            for link in links:
+                # delete ports linked switches
+                self.port_info[link.src.dpid].discard(link.src.port_no)
+                self.port_info[link.dst.dpid].discard(link.dst.port_no)
+
+                # s1 -> s2: s1.port, s2 -> s1: s2.port
+                self.port_link[(link.src.dpid,link.src.port_no)]=(link.src.dpid, link.dst.dpid)
+                self.port_link[(link.dst.dpid,link.dst.port_no)] = (link.dst.dpid, link.src.dpid)
+
+                self.link_info[(link.src.dpid, link.dst.dpid)] = link.src.port_no
+                self.link_info[(link.dst.dpid, link.src.dpid)] = link.dst.port_no
+                
+
+                self.topo_map.add_edge(link.src.dpid, link.dst.dpid, hop=1, is_host=False)
 
     def shortest_path(self, src, dst, weight='hop'):
         try:
@@ -116,8 +248,8 @@ class NetworkAwareness(app_manager.OSKenApp):
 
     def show_topo_map(self):
         self.logger.info('topo map:')
-        self.logger.info('{:^10s}  ->  {:^10s}'.format('node', 'node'))
+        self.logger.info('{:^10s}  ->  {:^10s}      {:^10s}'.format('node', 'node', 'delay'))
         for src, dst in self.topo_map.edges:
-            self.logger.info('{:^10s}      {:^10s}'.format(str(src), str(dst)))
+            self.logger.info('{:^10s}      {:^10s}      {:^10s}'.format(str(src), str(dst), f"{self.topo_map.edges[src,dst].get('delay', -0.001)*1000:.0f}ms"))
         self.logger.info('\n')
 
--- shortest_path.py
+++ shortest_path.py
@@ -6,28 +6,35 @@ from os_ken.controller.handler import set_ev_cls
 from os_ken.controller.handler import set_ev_cls
 from os_ken.ofproto import ofproto_v1_3
 from os_ken.lib.packet import packet
-from os_ken.lib.packet import ethernet, arp, ipv4
+from os_ken.lib.packet import ethernet, arp, ipv4, lldp
 from os_ken.controller import ofp_event
 from os_ken.topology import event
+from os_ken.topology.switches import LLDPPacket
 import sys
 from network_awareness import NetworkAwareness
 import networkx as nx
+from os_ken.base.app_manager import lookup_service_brick
+
 ETHERNET = ethernet.ethernet.__name__
 ETHERNET_MULTICAST = "ff:ff:ff:ff:ff:ff"
 ARP = arp.arp.__name__
-class LeastHops(app_manager.OSKenApp):
+class ShortestPath(app_manager.OSKenApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
     _CONTEXTS = {
         'network_awareness': NetworkAwareness
     }
 
     def __init__(self, *args, **kwargs):
-        super(LeastHops, self).__init__(*args, **kwargs)
+        super(ShortestPath, self).__init__(*args, **kwargs)
         self.network_awareness = kwargs['network_awareness']
-        self.weight = 'hop'
+        # 权重改为delay
+        self.weight = 'delay'
         self.mac_to_port = {}
         self.sw = {}
-        self.path=None
+        # 存下当前有效的所有路径，当链路变化的时候通知交换机
+        self.path = []
+        self.switches = None
+        self.network_awareness.weight = self.weight
 
     def add_flow(self, datapath, priority, match, actions, idle_timeout=0, hard_timeout=0):
         dp = datapath
@@ -42,14 +49,46 @@ class LeastHops(app_manager.OSKenApp):
             match=match, instructions=inst)
         dp.send_msg(mod)
 
+    def delete_flow(self, datapath, port):
+        '''
+        对datapath删除包含指定port的所有流表项
+        '''
+        ofp = datapath.ofproto
+        parser = datapath.ofproto_parser
+        # 入端口为port的流表项
+        mod = parser.OFPFlowMod(
+            command=ofp.OFPFC_DELETE,
+            datapath=datapath, match=parser.OFPMatch(in_port=port),
+            out_port=ofp.OFPP_ANY, out_group=ofp.OFPG_ANY,
+        )
+        datapath.send_msg(mod)
+        
+        # 出端口为port的流表项
+        mod = parser.OFPFlowMod(
+            command=ofp.OFPFC_DELETE,
+            datapath=datapath, match=parser.OFPMatch(),
+            out_port=port, out_group=ofp.OFPG_ANY,
+        )
+        datapath.send_msg(mod)
+
+    def delete_flow_priority(self, datapath):
+        '''
+        对datapath删除包含指定port的所有流表项
+        '''
+        ofp = datapath.ofproto
+        parser = datapath.ofproto_parser
+        # 入端口为port的流表项
+        mod = parser.OFPFlowMod(
+            command=ofp.OFPFC_DELETE,
+            datapath=datapath, match=parser.OFPMatch(),
+            out_port=ofp.OFPP_ANY, out_group=ofp.OFPG_ANY,
+            priority=1,
+        )
+        datapath.send_msg(mod)
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
         msg = ev.msg
-        dp = msg.datapath
-        ofp = dp.ofproto
-        parser = dp.ofproto_parser
-
-        dpid = dp.id
         in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
@@ -62,7 +101,6 @@ class LeastHops(app_manager.OSKenApp):
         # layer 2 self-learning
         dst_mac = eth_pkt.dst
         src_mac = eth_pkt.src
-
 
         if isinstance(arp_pkt, arp.arp):
             self.handle_arp(msg, in_port, dst_mac,src_mac, pkt,pkt_type)
@@ -94,7 +132,7 @@ class LeastHops(app_manager.OSKenApp):
             actions = [parser.OFPActionOutput(self.mac_to_port[dpid][dst])]   # 如果已学习，则向指定端⼝转发数据包 
             match = parser.OFPMatch(eth_dst=dst)  
             # 设置流表及其超时时间，使之能够适应拓扑变化
-            self.add_flow(dp, 1, match, actions)
+            self.add_flow(dp, 1, match, actions, 10, 30)
         out = parser.OFPPacketOut(
             datapath=dp, buffer_id=msg.buffer_id, in_port=msg.match['in_port'],actions=actions, data=msg.data)
         dp.send_msg(out)
@@ -108,7 +146,7 @@ class LeastHops(app_manager.OSKenApp):
 
 
 
-        self.path=dpid_path
+        self.path.append(dpid_path)
         # get port path:  h1 -> in_port, s1, out_port -> h2
         port_path = []
         for i in range(1, len(dpid_path) - 1):
@@ -117,8 +155,13 @@ class LeastHops(app_manager.OSKenApp):
             port_path.append((in_port, dpid_path[i], out_port))
         self.show_path(src_ip, dst_ip, port_path)
         # calc path delay
-
-
+        delay = 0
+        for i in range(1, len(dpid_path) - 2):
+            src = dpid_path[i]
+            dst = dpid_path[i+1]
+            delay += self.network_awareness.topo_map[src][dst]['delay']
+        print(f"delay: {delay*1000:.0f}ms")
+        print(f"RTT: {delay*2000:.0f}ms")
         # send flow mod
         for node in port_path:
             in_port, dpid, out_port = node
@@ -132,13 +175,69 @@ class LeastHops(app_manager.OSKenApp):
         out = parser.OFPPacketOut(
             datapath=dp, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=msg.data)
         dp.send_msg(out)
+    
+    @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
+    def port_status_handler(self, ev):
+        '''
+        处理链路端口变化
+        '''
+        msg = ev.msg
+        dp = msg.datapath
+        port = msg.desc.port_no
+        reason = msg.reason
+        ofproto = dp.ofproto
+        # 若端口消失
+        if reason == ofproto.OFPPR_DELETE or (reason == ofproto.OFPPR_MODIFY and msg.desc.state in [ofproto.OFPPS_LINK_DOWN, ofproto.OFPPS_BLOCKED]):
+            # 获取错误的路径
+            error_path = []
+            for dpid_path in self.path:
+                for i in range(1, len(dpid_path) - 1):
+                    in_port = self.network_awareness.link_info[(dpid_path[i], dpid_path[i - 1])]
+                    out_port = self.network_awareness.link_info[(dpid_path[i], dpid_path[i + 1])]
+                    if dpid_path[i] == dp.id and port in [in_port, out_port]:
+                        error_path.append(dpid_path)
+            # 删除这个路径，并通知交换机删除流表项
+            for dpid_path in error_path:
+                self.path.remove(dpid_path)
+                for i in range(1, len(dpid_path) - 1):
+                    in_port = self.network_awareness.link_info[(dpid_path[i], dpid_path[i - 1])]
+                    out_port = self.network_awareness.link_info[(dpid_path[i], dpid_path[i + 1])]
+                    self.delete_flow(self.network_awareness.switch_info[dpid_path[i]], in_port)
+                    self.delete_flow(self.network_awareness.switch_info[dpid_path[i]], out_port)
+
+            # 删除ARP相关项
+            to_discard = []
+            for key in self.sw:
+                dpid, _, _ = key
+                if dpid == dp.id and self.sw[key] == port: 
+                    to_discard.append(key)
+            for key in to_discard:
+                self.sw.pop(key, None)
+            
+            to_discard = []
+            self.mac_to_port = {}
+            self.sw = {}
+        
+        # 若端口新增
+        else:
+            # 通知交换机删除所有流表项，让其重新发消息到控制器获取新的最短路径。
+            for dpid_path in self.path:
+                self.path.remove(dpid_path)
+                for i in range(1, len(dpid_path) - 1):
+                    in_port = self.network_awareness.link_info[(dpid_path[i], dpid_path[i - 1])]
+                    out_port = self.network_awareness.link_info[(dpid_path[i], dpid_path[i + 1])]
+                    self.delete_flow(self.network_awareness.switch_info[dpid_path[i]], in_port)
+                    self.delete_flow(self.network_awareness.switch_info[dpid_path[i]], out_port)
+        # 调用network_awareness的方法让其应对变化
+        self.network_awareness.port_status_handler(ev)
+
 
     def send_flow_mod(self, parser, dpid, pkt_type, src_ip, dst_ip, in_port, out_port):
         dp = self.network_awareness.switch_info[dpid]
         match = parser.OFPMatch(
             in_port=in_port, eth_type=pkt_type, ipv4_src=src_ip, ipv4_dst=dst_ip)
         actions = [parser.OFPActionOutput(out_port)]
-        self.add_flow(dp, 1, match, actions, 10, 30)
+        self.add_flow(dp, 2, match, actions, 10, 30)
 
     def show_path(self, src, dst, port_path):
         self.logger.info('path: {} -> {}'.format(src, dst))
